86848e908566548198e63e1b031d454ecc76c936
이지은 <wlsl8302@naver.com>
2024-12-26 20:16:33 +0900

    common css styles

--- marketkurly/src/main.jsx
+++ marketkurly/src/main.jsx
@@ -1,8 +1,12 @@
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-
+  // BrowserRouter는 현재 주소를 저장하고 감지하는 역할을 한다
+  //리액트 앱의 모든 컴포넌트들이 이렇게 페이지 라우팅과 관련된 모든 데이터를 공급받아 사용가능
+  <BrowserRouter>
     <App />
-  
-)
+  </BrowserRouter>
+);
