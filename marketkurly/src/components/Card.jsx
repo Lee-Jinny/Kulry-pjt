458b1eb85fb8bae4da4201a5e2f0bd61930fbca0
정수연 <wjdtndus23@naver.com>
2025-01-06 01:09:29 +0900

    cardlist 작성

--- marketkurly/src/components/Card.jsx
+++ marketkurly/src/components/Card.jsx
@@ -1,7 +1,42 @@
+import React from 'react';
+import './card.css';  // card.css 파일을 임포트
+
 const Card = ({ item }) => {
   return (
-    <p>각각 카드 낱장</p>
+    <div className="card-container">
+      {/* 상품 이미지 */}
+      <img
+        src={item.image_path}
+        alt={item.product_name}
+        className="card-image"  // 이미지 스타일 적용
+      />
+      {/* 장바구니 버튼 */}
+      <button className="card-button">
+        🛒 담기
+      </button>
+      {/* 상품 이름 */}
+      <h3 className="card-name">
+        {item.product_name}
+      </h3>
+      {/* 기존 가격, price_salerate가 있는 경우에만 표시 */}
+      {item.details.price_salerate && item.details.price_salerate !== "" && (
+        <p className="card-price-prev">
+          {item.details.price_prev}
+        </p>
+      )}
+      {/* 할인율과 할인된 가격 */}
+      <p className="card-price-info">
+        {item.details.price_salerate && (
+          <span className="card-price-salerate">
+            {item.details.price_salerate}
+          </span>
+        )}
+        <span className="card-price-main">
+          {item.details.price_main}원
+        </span>
+      </p>
+    </div>
   );
-}
+};
 
 export default Card;
--- marketkurly/src/components/Cardlist.jsx
+++ marketkurly/src/components/Cardlist.jsx
@@ -1,7 +1,39 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import products from "../kulry_products.json"; 
+import Card from './Card';  // Card 컴포넌트 임포트
+import './Cardlist.css'
+
 const Cardlist = () => {
-  return (
-    <p>card모음집입니다</p>
+  const filteredProducts = products.filter(
+    (product) => product.category_number === 910
   );
-}
+
+  return (
+    <>
+      <div className="title">
+        <h2 className="main_title">👑올해 마지막 컬리픽 특가</h2>
+        <p className="sub_title">놓치지 말고 담아가세요.</p>
+      </div>
+
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={4}
+        navigation
+        loop={true}
+        modules={[Navigation]}
+      >
+        {filteredProducts.map((product) => (
+          <SwiperSlide key={product.id}>
+            <Card item={product} />  {/* Card 컴포넌트에 product 전달 */}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
 
 export default Cardlist;
