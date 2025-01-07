6d3f3ab88bb2f65642ccb94908135181e8de903f
정수연 <wjdtndus23@naver.com>
2025-01-07 17:42:48 +0900

    addd

--- marketkurly/src/components/Lankingcard.jsx
+++ marketkurly/src/components/Lankingcard.jsx
@@ -1,13 +1,45 @@
+import React from 'react';
+import './Lankingcard.css'; 
+
 const Lankingcard = ({ item }) => {
   return (
-    <div className="lankingcard">
-      <img src={item.img} alt={item.name} />
-      <div className="lankingcard__info">
-        <h3>{item.name}</h3>
-        <p>{item.description}</p>
-        <p>{item.price}</p>
-      </div>
+    <div className="lankingcard-container">
+      {/* 상품 이미지 */}
+      <img
+        src={item.image_path}
+        alt={item.product_name}
+        className="lankingcard-image"  // 이미지 스타일 적용
+      />
+      <h2>{item.id+1}</h2>
+      {/* 상품 이름 */}
+      <h3 className="lankingcard-name">
+        {item.product_name}
+      </h3>
+      {/* 기존 가격, price_salerate가 있는 경우에만 표시 */}
+      {item.details.price_salerate && item.details.price_salerate !== "" && (
+        <p className="lankingcard-price-prev">
+          {item.details.price_prev}
+        </p>
+      )}
+      
+      {/* 할인율과 할인된 가격 */}
+      <p className="lankingcard-price-info">
+        {item.details.price_salerate && (
+          <span className="lankingcard-price-salerate">
+            {item.details.price_salerate}
+          </span>
+        )}
+        <span className="lankingcard-price-main">
+          {item.details.price_main}원
+        </span>
+      </p>
+      {/* 장바구니 버튼 */}
+      <button className="lankingcard-button">
+        🛒 담기
+      </button>
     </div>
   );
-}
+};
+
+export default Lankingcard;
 
--- marketkurly/src/components/Lankinglist.jsx
+++ marketkurly/src/components/Lankinglist.jsx
@@ -1,9 +1,36 @@
-const Langkinglist = () => { 
-  return (
-    <div>
-      <h1>랭킹 리스트</h1>
-    </div>
-  );
-} 
+import React from "react";
+import Lankingcard from "./Lankingcard";
+import './Lankinglist.css'; 
+import products from "../kulry_products.json";
 
-export default Langkinglist;
+const Lankingcardlist = () => {
+  const filteredProducts = products.filter(
+    (product) => product.category_number === 907
+  );
+
+  return (
+    <>
+      <div>
+        <h2>오늘의 카테고리 랭킹</h2>
+        <div className="category_list">
+          <p>간편식Top50</p>
+          <p>간식Top50</p>
+          <p>베이커리Top50</p>
+          <p>반찬Top50</p>
+          <p>유제품Top50</p>
+          <p>주방Top50</p>
+          <p>뷰티Top50</p>
+        </div>
+        <div className="lanking-card-list">
+          {filteredProducts.map((product) => (
+            <Lankingcard key={product.id} item={product} />
+          ))}
+        </div>
+      </div>
+      
+    </>
+  );
+};
+
+export default Lankingcardlist;
+
